2240d60bd03c12745e360fd40115afcfad5f7432
DANIEL LIABEUF <daniel.liabeuf@gmail.com>
2017-12-15 21:48:53 +0100

    Optimization (factor 10, thanks to developpez.net).  - removal of floating point operations  - improvement of navigation in the cube  - decrease in the number of readings in the cube (4 points instead of 8).

--- Array3DLUT.h
+++ Array3DLUT.h
@@ -6,6 +6,7 @@
 
 FASTLED_NAMESPACE_BEGIN
 
+
 /// 3D array index
 struct LUTCoord {
 	union {
@@ -27,39 +28,15 @@ struct LUTCoord {
     }
 };
 
-/// 8 vertices of a cube
-struct LUTSubcube {
-	CRGB n000;
-	CRGB n001;
-	CRGB n010;
-	CRGB n011;
-	CRGB n100;
-	CRGB n101;
-	CRGB n110;
-	CRGB n111;
-};
 
 /// This 3D LUT can be used provided that the space between entries is the same for all dimensions.
+template<uint8_t SIZE>
 class Array3DLUT {
 public:
-	CRGB *entries;
-	uint8_t size;
-	float step;
-	uint8_t maxSubcubeIndex;
+	CRGB (*cube)[SIZE][SIZE];
 
-	Array3DLUT(CRGB *entries, uint8_t size);
+	Array3DLUT(CRGB entries[SIZE][SIZE][SIZE]);
 	virtual ~Array3DLUT();
-
-	/// Retrieve an entry in the table.
-	/// @param indexes - 3D index of entry
-	/// @returns Entry value
-	CRGB lookup(LUTCoord indexes);
-
-	/// Returns the values of the 8 vertices of the target sub-cube.
-	/// The given coordinates must be those of the lowest point.
-	/// @param subcubeOriginCoord - lowest point coordinates of the desired subcube
-	/// @returns Each (8) subcube values
-	struct LUTSubcube lookup_subcube(LUTCoord subcubeOriginCoord);
 
 	/// Similar to lookup but instead of a coordinate in the LUT,
 	/// we pass a point and the result will be interpolated.
@@ -67,5 +44,113 @@ public:
 	/// @returns Interpolated value in the LUT
 	struct CRGB lookup3DTetrahedral(CRGB &input);
 };
+
+
+
+
+template<uint8_t SIZE>
+Array3DLUT<SIZE>::Array3DLUT(CRGB (*entries)[SIZE][SIZE]) {
+	this->cube = entries;
+}
+
+template<uint8_t SIZE>
+Array3DLUT<SIZE>::~Array3DLUT() {
+	// TODO Auto-generated destructor stub
+}
+
+// Ref implem: https://github.com/ampas/CLF/blob/master/python/aces/clf/Array.py
+template<uint8_t SIZE>
+struct CRGB Array3DLUT<SIZE>::lookup3DTetrahedral(CRGB &input) {
+	LUTCoord subcubeDarkestVertice; // Store the subcube's origin coordinates
+	uint8_t darkSpotDistances[3]; // Distance input <-> subcube's darkest vertice
+
+	#define STEP (255/(SIZE-1))
+	#define MAX_INDEX SIZE-2
+	for (int i = 0; i < 3; i++) { // for each rgb channel
+		subcubeDarkestVertice.raw[i] = input.raw[i] / STEP;
+		darkSpotDistances[i] = input.raw[i] % STEP;
+
+		if (subcubeDarkestVertice.raw[i] > MAX_INDEX) {
+			// But, if we are on an upper border then we must take the
+			// inner border or the subcube will not exist.
+			subcubeDarkestVertice.raw[i] = MAX_INDEX;
+			darkSpotDistances[i] = 255;
+		}
+	}
+
+	// Rebind for consistency with Truelight paper
+	#define fx darkSpotDistances[0]
+	#define fy darkSpotDistances[1]
+	#define fz darkSpotDistances[2]
+	#define N000 this->cube[subcubeDarkestVertice.redIndex  ][subcubeDarkestVertice.greenIndex  ][subcubeDarkestVertice.blueIndex  ]
+	#define N001 this->cube[subcubeDarkestVertice.redIndex  ][subcubeDarkestVertice.greenIndex  ][subcubeDarkestVertice.blueIndex+1]
+	#define N010 this->cube[subcubeDarkestVertice.redIndex  ][subcubeDarkestVertice.greenIndex+1][subcubeDarkestVertice.blueIndex  ]
+	#define N011 this->cube[subcubeDarkestVertice.redIndex  ][subcubeDarkestVertice.greenIndex+1][subcubeDarkestVertice.blueIndex+1]
+	#define N100 this->cube[subcubeDarkestVertice.redIndex+1][subcubeDarkestVertice.greenIndex  ][subcubeDarkestVertice.blueIndex  ]
+	#define N101 this->cube[subcubeDarkestVertice.redIndex+1][subcubeDarkestVertice.greenIndex  ][subcubeDarkestVertice.blueIndex+1]
+	#define N110 this->cube[subcubeDarkestVertice.redIndex+1][subcubeDarkestVertice.greenIndex+1][subcubeDarkestVertice.blueIndex  ]
+	#define N111 this->cube[subcubeDarkestVertice.redIndex+1][subcubeDarkestVertice.greenIndex+1][subcubeDarkestVertice.blueIndex+1]
+
+	struct CRGB result;
+	if (fx > fy) {
+		if (fy > fz) {
+			for (int i=0; i < 3; i++) {
+				result.raw[i] = (
+						(uint16_t)(255-fx) * N000.raw[i] +
+						(uint16_t)(fx-fy)  * N100.raw[i] +
+						(uint16_t)(fy-fz)  * N110.raw[i] +
+						(uint16_t)(fz)     * N111.raw[i] ) >> 8;
+			}
+		} else {
+			if (fx > fz) {
+				for (int i=0; i < 3; i++) {
+					result.raw[i] = (
+							(uint16_t)(255-fx) * N000.raw[i] +
+							(uint16_t)(fx-fz)  * N100.raw[i] +
+							(uint16_t)(fz-fy)  * N101.raw[i] +
+							(uint16_t)(fy)     * N111.raw[i] ) >> 8;
+				}
+			} else {
+				for (int i=0; i < 3; i++) {
+					result.raw[i] = (
+							(uint16_t)(255-fz) * N000.raw[i] +
+							(uint16_t)(fz-fx)  * N001.raw[i] +
+							(uint16_t)(fx-fy)  * N101.raw[i] +
+							(uint16_t)(fy)     * N111.raw[i] ) >> 8;
+				}
+			}
+		}
+	} else {
+		if (fz > fy) {
+			for (int i=0; i < 3; i++) {
+				result.raw[i] = (
+						(uint16_t)(255-fz) * N000.raw[i] +
+						(uint16_t)(fz-fy)  * N001.raw[i] +
+						(uint16_t)(fy-fx)  * N011.raw[i] +
+						(uint16_t)(fx)     * N111.raw[i] ) >> 8;
+			}
+		} else {
+			if (fz > fx) {
+				for (int i=0; i < 3; i++) {
+					result.raw[i] = (
+							(uint16_t)(255-fy) * N000.raw[i] +
+							(uint16_t)(fy-fz)  * N010.raw[i] +
+							(uint16_t)(fz-fx)  * N011.raw[i] +
+							(uint16_t)(fx)     * N111.raw[i] ) >> 8;
+				}
+			} else {
+				for (int i=0; i < 3; i++) {
+					result.raw[i] = (
+							(uint16_t)(255-fy) * N000.raw[i] +
+							(uint16_t)(fy-fx)  * N010.raw[i] +
+							(uint16_t)(fx-fz)  * N011.raw[i] +
+							(uint16_t)(fz)     * N111.raw[i] ) >> 8;
+				}
+			}
+		}
+	}
+
+	return result;
+}
 
 #endif /* ARRAY3DLUT_H_ */
